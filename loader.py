6f78994afdb3165aa78d291d76f30ebcbc83650b
ShiXiaozhou <1459280722@qq.com>
2020-01-16 21:27:27 +0800

    feat(predictor):the first version of predictor, need to be modified

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import pyreadr as py # Library to read .Rdata files in python
+import pyreadr as py  # Library to read .Rdata files in python
+
 
 def load_RData():
     # Reading train data in .R format
@@ -12,13 +13,14 @@ def load_RData():
 
     # Concatinating the train and the test dataset
     tr = [train_data_0['fault_free_training'], train_data_1['faulty_training']]
-    train = pd.concat(tr) # Train dataframe
+    train = pd.concat(tr)  # Train dataframe
     ts = [test_data_0['fault_free_testing'], test_data_1['faulty_testing']]
-    test = pd.concat(ts) # Test dataframe
+    test = pd.concat(ts)  # Test dataframe
 
     # Save the datasets into csv file
     train.to_csv("data/train.csv")
     test.to_csv("data/test.csv")
+
 
 # Read csv data
 train_data = pd.read_csv("data/train.csv")
@@ -28,13 +30,13 @@ print('finish reading csv files!')
 print('Shape of the Train dataset:', train_data.shape)
 print("Shape of the Test dataset:", test_data.shape)
 
-Sampled_train = pd.DataFrame() # dataframe to store the train dataset
-Sampled_test = pd.DataFrame() # dataframe to store test 
-Sampled_cv = pd.DataFrame() # dataframe to store cv data
+Sampled_train = pd.DataFrame()  # dataframe to store the train dataset
+Sampled_test = pd.DataFrame()  # dataframe to store test
+Sampled_cv = pd.DataFrame()  # dataframe to store cv data
 
 # Program to construct the sample train data
 frame = []
-for i in set(train_data['faultNumber']): 
+for i in set(train_data['faultNumber']):
     b_i = pd.DataFrame()
     if i == 0:
         b_i = train_data[train_data['faultNumber'] == i][0:20000]
@@ -42,13 +44,13 @@ for i in set(train_data['faultNumber']):
     else:
         fr = []
         b = train_data[train_data['faultNumber'] == i]
-        for x in range(1,25):
+        for x in range(1, 25):
             b_x = b[b['simulationRun'] == x][20:500]
             fr.append(b_x)
-        
+
         b_i = pd.concat(fr)
-        
-    frame.append(b_i)      
+
+    frame.append(b_i)
 Sampled_train = pd.concat(frame)
 
 # Program to construct the sample CV Data
@@ -61,13 +63,13 @@ for i in set(train_data['faultNumber']):
     else:
         fr = []
         b = train_data[train_data['faultNumber'] == i]
-        for x in range(26,35):
+        for x in range(26, 35):
             b_x = b[b['simulationRun'] == x][20:500]
             fr.append(b_x)
 
         b_i = pd.concat(fr)
-        
-    frame.append(b_i)      
+
+    frame.append(b_i)
 Sampled_cv = pd.concat(frame)
 
 # Program to construct Sampled Test data
@@ -80,13 +82,13 @@ for i in set(test_data['faultNumber']):
     else:
         fr = []
         b = test_data[test_data['faultNumber'] == i]
-        for x in range(1,11):
+        for x in range(1, 11):
             b_x = b[b['simulationRun'] == x][160:660]
             fr.append(b_x)
-        
+
         b_i = pd.concat(fr)
-        
-    frame.append(b_i)      
+
+    frame.append(b_i)
 Sampled_test = pd.concat(frame)
 print("Finish sample data!")
 
