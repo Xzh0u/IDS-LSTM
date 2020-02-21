81f7ccb5330ef25d55b074aab84f671c581bc908
Xzh0u <littleland.sxz@gmail.com>
2020-02-21 20:03:11 +0800

    feat(log): add the function of record log in a file

--- trainer.py
+++ trainer.py
@@ -3,10 +3,17 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import numpy as np
 import pandas as pd
-from utils import to_categorical
+import logging
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Prepare logger
+logging.basicConfig(filename='saved/log/checkpoint.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
 
 # Hyper-parameters
 input_size = 52
@@ -22,10 +29,6 @@ train = pd.read_csv("data/sampled/train.csv")
 test = pd.read_csv("data/sampled/test.csv")
 cv = pd.read_csv("data/sampled/cv.csv")
 
-print("Shape of the sampled train data:", train.shape)
-print("Shape of the sampled test data:", test.shape)
-print("Shape of the sampled CV data:", cv.shape)
-
 # Sorting the Datasets w.r.t. to the simulation runs
 train.sort_values(by=['simulationRun', 'faultNumber'], inplace=True)
 test.sort_values(by=['simulationRun', 'faultNumber'], inplace=True)
@@ -38,15 +41,12 @@ ts = test.drop(test[(test.faultNumber == 3) | (test.faultNumber == 9) | (
     test.faultNumber == 15)].index).reset_index()
 cv_ = cv.drop(cv[(cv.faultNumber == 3) | (cv.faultNumber == 9)
                  | (cv.faultNumber == 15)].index).reset_index()
-print(tr)
+logging.debug("Raw dataset:{}".format(tr))
 
 y_train = tr['faultNumber']
 y_test = ts['faultNumber']
 y_cv = cv_['faultNumber']
 
-# y_train = to_categorical(tr['faultNumber'], num_classes=21)
-# y_test = to_categorical(ts['faultNumber'], num_classes=21)
-# y_cv = to_categorical(cv_['faultNumber'], num_classes=21)
 
 # Removing unnecessary features from train, test and cv data.
 tr.drop(['faultNumber', 'Unnamed: 0', 'Unnamed: 0.1',
@@ -55,17 +55,16 @@ ts.drop(['faultNumber', 'Unnamed: 0', 'Unnamed: 0.1',
          'simulationRun', 'sample', 'index'], axis=1, inplace=True)
 cv_.drop(['faultNumber', 'Unnamed: 0', 'Unnamed: 0.1',
           'simulationRun', 'sample', 'index'], axis=1, inplace=True)
-print(tr)
+logging.debug("Dataset which will be used:{}".format(tr))
 
 # Data normalization
 train_normalized = (tr - np.mean(tr)) / np.std(tr)
 test_normalized = (ts - np.mean(ts)) / np.std(ts)
 cv_normalized = (cv_ - np.mean(cv_)) / np.std(cv_)
-print(train_normalized)
 
-# print('Shape of the Train dataset:', train_normalized.shape)
-# print("Shape of the Test dataset:", test_normalized.shape)
-# print("Shape of the CV dataset:", cv_normalized.shape)
+logging.debug("Shape of the Train dataset: {}".format(train_normalized.shape))
+logging.debug("Shape of the Test dataset: {}".format(train_normalized.shape))
+logging.debug("Shape of the CV dataset: {}".format(train_normalized.shape))
 
 # Resizing the train, test and cv data.
 x_train = np.resize(train_normalized, (230400, 1, 52))
@@ -84,7 +83,7 @@ test_loader = torch.utils.data.DataLoader(dataset=x_test,
 cv_loader = torch.utils.data.DataLoader(dataset=x_cv,
                                         batch_size=batch_size,
                                         shuffle=True)
-print("DataLoader prepared!")
+logging.debug("DataLoader prepared!")
 
 # build the network
 
@@ -127,8 +126,8 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 total_step = len(train_loader)
-print(total_step)
-print(len(test_loader))
+logging.debug("Total step is: %d", total_step)
+logging.debug("Lenth of the test loader is: %d", len(test_loader))
 for epoch in range(num_epochs):
     for i, datas in enumerate(train_loader):
         # Forward pass
@@ -145,8 +144,8 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
 
         if (i+1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                  .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+            logging.info('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                         .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
 # Test the model
 with torch.no_grad():
@@ -161,8 +160,8 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-    print('Test Accuracy of the model on test examples: {} %'.format(
+    logging.info('Test Accuracy of the model on test examples: {} %'.format(
         100 * correct / total))
 
 # Save the model checkpoint
-torch.save(model.state_dict(), 'model.pkl')
+torch.save(model.state_dict(), 'saved/model.pkl')
--- utils.py
+++ utils.py
@@ -1,45 +1,36 @@
-import numpy as np
-
-def to_categorical(y, num_classes=None, dtype='float32'):
-    """Converts a class vector (integers) to binary class matrix.
-    E.g. for use with categorical_crossentropy.
-    # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-        dtype: The data type expected by the input, as a string
-            (`float32`, `float64`, `int32`...)
-    # Returns
-        A binary matrix representation of the input. The classes axis
-        is placed last.
-    # Example
-    ```python
-    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
-    > labels
-    array([0, 2, 1, 2, 0])
-    # `to_categorical` converts this into a matrix with as many
-    # columns as there are classes. The number of rows
-    # stays the same.
-    > to_categorical(labels)
-    array([[ 1.,  0.,  0.],
-           [ 0.,  0.,  1.],
-           [ 0.,  1.,  0.],
-           [ 0.,  0.,  1.],
-           [ 1.,  0.,  0.]], dtype=float32)
-    ```
-    """
-    y = np.array(y, dtype='int')
-    input_shape = y.shape
-    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-        input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
-    if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=dtype)
-    categorical[np.arange(n), y] = 1
-    output_shape = input_shape + (num_classes,)
-    categorical = np.reshape(categorical, output_shape)
-    return categorical
+import json
+from pathlib import Path
+import pandas as pd
+from collections import OrderedDict
 
 
+def read_json(fname):
+    fname = Path(fname)
+    with fname.open('rt') as handle:
+        return json.load(handle, object_hook=OrderedDict)
+
+
+class MetricTracker:
+    def __init__(self, *keys, writer=None):
+        self.writer = writer
+        self._data = pd.DataFrame(
+            index=keys, columns=['total', 'counts', 'average'])
+        self.reset()
+
+    def reset(self):
+        for col in self._data.columns:
+            self._data[col].values[:] = 0
+
+    def update(self, key, value, n=1):
+        if self.writer is not None:
+            self.writer.add_scalar(key, value)
+        self._data.total[key] += value * n
+        self._data.counts[key] += n
+        self._data.average[key] = self._data.total[key] / \
+            self._data.counts[key]
+
+    def avg(self, key):
+        return self._data.average[key]
+
+    def result(self):
+        return dict(self._data.average)
